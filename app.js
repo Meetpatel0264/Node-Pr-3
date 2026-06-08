a03ee97c4cb77747fced973b66e5c2b3bbd459c8
Meet Patel <meetpatel.dev45@gmail.com>
2026-06-08 09:47:53 +0530

    Refactor express.urlencoded middleware usage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
     res.render("pages/home", {
